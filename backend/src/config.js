a8fef2b222d54a67e567e773b988b13fb7f26d4e
danggro <anangdwin55@gmail.com>
2023-12-29 10:44:46 +0700

    add images

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,7 +2,11 @@ const { Sequelize } = require("sequelize");
 const { createClient } = require("redis");
 
 const sequelize = new Sequelize(process.env.POSTGRES_DB, { logging: false }); // Example for postgres
-
+// const sequelize = new Sequelize("test", "postgres", "Danggro@120300", {
+//   host: "mcdt.postgres.database.azure.com",
+//   dialect: "postgres",
+//   port: 5432,
+// });
 const clientRedis = createClient({
   url: process.env.REDIS_DB,
 });
--- backend/src/postgres_db/index.js
+++ backend/src/postgres_db/index.js
@@ -4,6 +4,7 @@ const { sequelize } = require("../config");
 // Option 1: Passing a connection URI
 const connectToDatabase = async () => {
   try {
+    console.log(process.env.POSTGRES_DB);
     await sequelize.authenticate();
     await User.sync();
     console.log("Connection has been established successfully.");
